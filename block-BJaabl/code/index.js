4834c3fe0508d9ba414827d68d2d5c4431b5da17
smithan kumar r <smithankumar404@gmail.com>
2022-04-15 11:47:15 +0530

    trying

--- block-BJaabl/code/index.js
+++ block-BJaabl/code/index.js
@@ -1,7 +1,10 @@
 /*
 1. Create a function named `sayHello` that that accepts a parameter `name` and alert `Hello [name]!`.
 */
-
+function sayHello(name) {
+  alert(`Hello ${name}`);
+}
+sayHello("janu");
 /*
 2. Create a function named `getFullName` that accepts two parameter `firstName` and `lastName` and returns
 fullName.
@@ -10,7 +13,10 @@ Example:
   getFullName("John", "Snow"); // "John Snow"
   getFullName("Nelson", "Mandela"); // "Nelson Mandela"
 */
-
+// function getFullName(firstName , lastName) {
+// return(`${firstName},${lastName}`);
+// }
+// getFullName("ram","murthy");
 /*
 3. Create a function named `addTwoNumbers` that accepts two numbers i.e `firstNum` and `secondNum` and returns
 the sum of both numbers. If user passes any data other then numbers alert a message saying `Enter Valid Input`
@@ -20,7 +26,15 @@ addTwoNumbers(10, 22); // 32
 addTwoNumbers(20, 32); // 32
 addTwoNumbers(10, "100"); // Alert Enter Valid Input
 */
-
+// function addTwoNumbers(firstNum,secondNum) {
+//   if(firstNum === Number || secondNum === Number) {
+//     let sum = (`${firstNum + secondNum}`);
+//     return(sum);
+//   }
+//   else if (firstNum === String || secondNum === String) {
+//     alert(`Enter Valid Input`)
+//   }
+// }
 /*
 4. Create a function named `calc` which accepts three parameter `numA`, `numB` and `operation`. Operation
 can only be one of these `add, sub, mul, div`. Based on the operator return the value. If the `numA` or `numB`
@@ -33,7 +47,23 @@ calc(20, 10, 'sub'); // 10
 calc(20, 10, 'mul'); // 200
 */
 
-
+function calc(numA, numB, operation) {
+  let operation = add, sub, mul, div;
+  switch (operation) {
+    case add: numA + numB;
+      break;
+    case sub: numA - numB;
+      break;
+    case mul: numA * numB;
+      break;
+    case div: numA / numB;
+      break;
+    default:
+      alert(`Enter Valid Input`);
+  }
+  return (calc);
+}
+calc(10,22,sub);
 /*
 5. Create a function named `isLeapYear` that accepts a number data type and return `true` or `false` based
 on if the year id leap year or not.
